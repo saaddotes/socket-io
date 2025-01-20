794be5edab50ad1491089f0455007e991f7f3eb4
cadetsaad <cadetsaadsoomro@gmail.com>
2025-01-21 01:11:58 +0500

    add socket logic

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ import Chat from "./models/ChatModel.js";
 import auth from "./routes/auth.js";
 import "dotenv/config";
 import cors from "cors";
+import User from "./models/user.js";
 
 const { MONGODB_URI, ORIGIN, PORT } = process.env;
 const app = express();
@@ -42,6 +43,78 @@ io.on("connection", async (socket) => {
       socket.to(receiverId).emit("message-receive", { senderId, message });
     }
   );
+
+  socket.on("add-friend", async ({ senderId, receiverEmail }, callback) => {
+    try {
+      const sender = await User.findById(senderId);
+      const receiver = await User.findOne({ email: receiverEmail });
+
+      console.log("Add friend => ", senderId, receiverEmail);
+
+      if (!sender || !receiver) {
+        return callback({
+          success: false,
+          message: "Invalid sender or receiver ID.",
+        });
+      }
+
+      receiver.friendRequests.push(sender._id);
+      await receiver.save();
+      const receiverSocketId = users[receiver._id];
+      console.log(
+        "receiver.socketId => ",
+        receiver.socketId,
+        "\nreceiverSocketId => ",
+        receiverSocketId
+      );
+      if (receiverSocketId) {
+        io.to(receiverSocketId).emit("trigger-requests", { receiver });
+      }
+
+      callback({ success: true, message: "Friend request sent successfully." });
+    } catch (error) {
+      console.error("Error in add-friend:", error);
+      callback({ success: false, message: "Failed to send friend request." });
+    }
+  });
+
+  socket.on("accept-request", async ({ senderId, requestId }, callback) => {
+    try {
+      const sender = await User.findById(senderId);
+      const receiver = await User.findById(requestId);
+
+      console.log("Add friend => ", senderId, requestId);
+
+      if (!sender || !receiver) {
+        return callback({
+          success: false,
+          message: "Invalid sender or receiver ID.",
+        });
+      }
+
+      sender.friendRequests.pull(receiver._id);
+
+      sender.friends.push(receiver._id);
+      receiver.friends.push(sender._id);
+      await sender.save();
+      await receiver.save();
+      const receiverSocketId = users[receiver._id];
+      const senderSocketId = users[sender._id];
+
+      if (receiverSocketId) {
+        io.to(receiverSocketId).emit("trigger-requests", { receiver });
+      }
+
+      if (senderSocketId) {
+        io.to(senderSocketId).emit("trigger-requests", { receiver: sender });
+      }
+
+      callback({ success: true, message: "Friend request sent successfully." });
+    } catch (error) {
+      console.error("Error in add-friend:", error);
+      callback({ success: false, message: "Failed to send friend request." });
+    }
+  });
 
   socket.on("disconnect", () => {
     if (userId) delete userId[userId];
