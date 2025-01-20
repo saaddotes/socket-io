b277c38b841d043073298c2715ec6ec4e867dd08
cadetsaad <cadetsaadsoomro@gmail.com>
2025-01-21 01:10:08 +0500

    showed list of users

--- next-client/src/components/chat/ChatSidebar.tsx
+++ next-client/src/components/chat/ChatSidebar.tsx
@@ -1,22 +1,9 @@
 "use client";
 
-import { useState, useEffect } from "react";
-import {
-  collection,
-  query,
-  where,
-  orderBy,
-  onSnapshot,
-  doc as firestoreDoc,
-  getDoc,
-  Timestamp,
-} from "firebase/firestore";
-import { db, auth } from "@/lib/firebase";
+import { useState } from "react";
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { motion } from "framer-motion";
-import CreateGroupChat from "./CreateGroupChat";
 import { AddMember } from "./AddMember";
 import {
   Dialog,
@@ -26,7 +13,6 @@ import {
   DialogTrigger,
 } from "../ui/dialog";
 import { UserSettings } from "../settings/UserSettings";
-import axios from "axios";
 import { useAuth } from "@/context/authContext";
 import FriendRequests from "./FriendsReqs";
 
@@ -37,7 +23,7 @@ export function ChatSidebar({
 }) {
   const [chats, setChats] = useState<any>([]);
   const [searchTerm, setSearchTerm] = useState("");
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
   const { user } = useAuth();
 
   // useEffect(() => {
@@ -48,7 +34,7 @@ export function ChatSidebar({
 
   // async function fetchMembers() {
   //   try {
-  //     const res = await axios.post("http://localhost:4000/auth/friends", {
+  //     const res = await axios.post("http://192.168.0.104:4000/auth/friends", {
   //       email: user?.email,
   //     });
   //     const data = await res.data;
@@ -76,17 +62,17 @@ export function ChatSidebar({
     <div className="w-full md:w-64 border-r border-gray-200 h-full flex flex-col">
       <div className="p-4">
         <div className="flex flex-col space-y-2 mb-2">
-          <FriendRequests user={user} />
+          <FriendRequests />
           <AddMember />
         </div>
       </div>
-      {/* <ScrollArea className="flex-1">
+      <ScrollArea className="flex-1">
         {loading ? (
           <div className="text-center text-gray-500">Loading chats...</div>
         ) : user && user.friends.length > 0 ? (
           user.friends.map((friend, index) => (
             <motion.div
-              key={friend._id}
+              key={friend}
               initial={{ opacity: 0, x: -20 }}
               animate={{ opacity: 1, x: 0 }}
               transition={{ duration: 0.3, delay: index * 0.1 }}
@@ -95,16 +81,16 @@ export function ChatSidebar({
               <Button
                 variant="secondary"
                 className="w-full justify-between h-16 rounded-lg"
-                onClick={() => onSelectChat(friend._id)}
+                onClick={() => onSelectChat(friend)}
               >
-                <div className="text-left">{friend.name}</div>
+                <div className="text-left">{friend}</div>
               </Button>
             </motion.div>
           ))
         ) : (
           <div className="text-center text-gray-500">No chats found.</div>
         )}
-      </ScrollArea> */}
+      </ScrollArea>
       <div className="px-4">
         <Dialog>
           <DialogTrigger asChild>
